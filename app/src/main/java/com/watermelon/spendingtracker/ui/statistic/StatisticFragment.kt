2853732fc66a9b06c4a1af7a29a58611bf151812
zahraa resen <zaawory98@gmail.com>
2021-11-23 21:03:28 +0300

    fix: change pie chart library

--- app/src/main/java/com/watermelon/spendingtracker/ui/statistic/StatisticFragment.kt
+++ app/src/main/java/com/watermelon/spendingtracker/ui/statistic/StatisticFragment.kt
@@ -1,4 +1,8 @@
+package com.watermelon.spendingtracker.ui.statistic
+
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.watermelon.spendingtracker.databinding.FragmentStatisticBinding
@@ -9,4 +13,11 @@ class StatisticFragment : BaseFragment<FragmentStatisticBinding>() {
     override val viewModel: HomeViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentStatisticBinding
         get() = FragmentStatisticBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        changeChartShape()
+    }
+
+    private fun changeChartShape (){
+    }
 }

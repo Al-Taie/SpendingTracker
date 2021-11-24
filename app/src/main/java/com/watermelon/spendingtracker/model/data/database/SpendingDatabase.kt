cb79378059a6d5b6185d84197300510a35ac9014
Ammar Thaar <amar.at909@gmail.com>
2021-11-24 18:18:04 +0300

    fix: temporary comment some classes and  files

--- app/src/main/java/com/watermelon/spendingtracker/model/data/database/SpendingDatabase.kt
+++ app/src/main/java/com/watermelon/spendingtracker/model/data/database/SpendingDatabase.kt
@@ -6,21 +6,27 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.watermelon.spendingtracker.model.data.Converters
-import com.watermelon.spendingtracker.model.data.domain.Category
-import com.watermelon.spendingtracker.model.data.domain.Spending
-import com.watermelon.spendingtracker.model.data.domain.SpendingType
-import com.watermelon.spendingtracker.model.data.domain.User
+import com.watermelon.spendingtracker.model.data.database.entities.Categories
+import com.watermelon.spendingtracker.model.data.database.entities.Salary
+import com.watermelon.spendingtracker.model.data.database.entities.User
+import com.watermelon.spendingtracker.model.data.database.relations.UserCategoriesCrossRef
 
 @Database(
-    entities = [User::class, Spending::class, SpendingType::class, Category::class],
+    entities = [
+        User::class,
+        Categories::class,
+        UserCategoriesCrossRef::class,
+        Salary::class
+
+    ],
     version = 1
 )
 @TypeConverters(Converters::class)
 abstract class SpendingDatabase : RoomDatabase() {
-    abstract val spendDao: SpendingDao
+   // abstract val spendDao: SpendingDao
     abstract val userDao: UserDao
     abstract val categoryDao: CategoryDao
-    abstract val spendingTypesDao: SpendingTypesDao
+    //  abstract val spendingTypesDao: SpendingTypesDao
 
     companion object {
         private const val DB_NAME = "DB_SPENDING"
--- app/src/main/java/com/watermelon/spendingtracker/model/data/domain/SpendingType.kt
+++ app/src/main/java/com/watermelon/spendingtracker/model/data/domain/SpendingType.kt
@@ -3,9 +3,9 @@ package com.watermelon.spendingtracker.model.data.domain
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "TB_SPENDING_TYPES")
-data class SpendingType (
-    @PrimaryKey(autoGenerate = false)
-    val id: Int,
-    val value: String
-)
+//@Entity(tableName = "TB_SPENDING_TYPES")
+//data class SpendingType (
+//    @PrimaryKey(autoGenerate = false)
+//    val id: Int,
+//    val value: String
+//)

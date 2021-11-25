93bfae630cd794bf7aa8be43288ba6b7f1c349fc
Ammar Thaar <amar.at909@gmail.com>
2021-11-25 03:23:03 +0300

    fix: user with categories relation done

--- app/src/main/java/com/watermelon/spendingtracker/model/data/database/entities/Categories.kt
+++ app/src/main/java/com/watermelon/spendingtracker/model/data/database/entities/Categories.kt
@@ -3,10 +3,10 @@ package com.watermelon.spendingtracker.model.data.database.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-//@Entity(tableName = "TB_CATEGORY")
-//
-//data class Categories(
-//    @PrimaryKey(autoGenerate = false)
-//    val categoriesId:Long,
-//    val categoriesName: String
-//)
+@Entity(tableName = "TB_CATEGORY")
+
+data class Categories(
+    @PrimaryKey(autoGenerate = false)
+    val categoriesId:Long,
+    val categoriesName: String
+)
--- app/src/main/java/com/watermelon/spendingtracker/model/data/database/relations/UserWithCategories.kt
+++ app/src/main/java/com/watermelon/spendingtracker/model/data/database/relations/UserWithCategories.kt
@@ -3,14 +3,15 @@ package com.watermelon.spendingtracker.model.data.database.relations
 import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
+import com.watermelon.spendingtracker.model.data.database.entities.Categories
 import com.watermelon.spendingtracker.model.data.database.entities.User
 
-//data class UserWithCategories(
-//    @Embedded val user: User,
-//    @Relation(
-//        parentColumn = "userId",
-//        entityColumn = "categoriesId",
-//        associateBy = Junction(UserCategoriesCrossRef::class)
-//    )
-//    val categories: List<Categories>
-//)
+data class UserWithCategories(
+    @Embedded val user: User,
+    @Relation(
+        parentColumn = "userId",
+        entityColumn = "categoriesId",
+        associateBy = Junction(UserCategoriesCrossRef::class)
+    )
+    val categories: List<Categories>
+)
